68641a249252b46f65d1ab6624a195ac00da29cf
Przemyslaw Kurczych <przemyslaw.kurczych@o2.pl>
2018-09-17 21:39:38 +0200

    Poprawiony blad odczytywania wspolrzednych

--- JXL-Converter/JXL.cs
+++ JXL-Converter/JXL.cs
@@ -14,6 +14,18 @@ namespace JXL_Converter
 {
     public partial class JXL : Form
     {
+        private double X1;
+        private double Y1;
+        private double H1;
+        private string Ref1Name;
+        private double X2;
+        private double Y2;
+        private double H2;
+        private double X0;
+        private double Y0;
+        private double H0;
+        private string Ref2Name;
+
         public JXL()
         {
             InitializeComponent();
@@ -36,8 +48,7 @@ namespace JXL_Converter
                 string fileName = ofd.FileName;
                 string filePath = Path.GetDirectoryName(fileName);
                 string onlyfileName = ofd.SafeFileName;
-                string PKTname;
-                string subPKTname = "_REF1";
+                string subPKTname = null;
                 string domiar2;
                 string domiar1;
                 string offset1 = "0";
@@ -52,47 +63,63 @@ namespace JXL_Converter
                     XmlNodeList itemlist = XmlDoc.SelectNodes("//FieldBook//PointRecord//Method");
                     foreach (XmlNode item in itemlist)
                     {
-                       if (item.InnerText == "FromBase")
+                        if (item.InnerText == "FromBase")
                         {
                             CountVRS++;
                         }
-                   }
+                    }
                     if (CountVRS != 0)
                     { CountVRS = 1; }
-                    
-                    int CountSum = CountVRS + CountGrid-CountPoint;
-                    for (int i = 0; i < CountPoint; i++)
+
+                    int CountSum = CountVRS + CountGrid - CountPoint;
+                    XmlNodeList reductionslist = XmlDoc.SelectNodes("//Reductions//Point");
+                    foreach (XmlNode reductions in reductionslist)
                     {
-                        PKTname = XmlDoc.GetElementsByTagName("Name").Item(i).InnerText;
-                        if (PKTname.Contains(subPKTname) == true)
+                        //  foreach (XmlNode point in reductions.ChildNodes) 
+                        if (reductions.ChildNodes.Item(1).InnerText.Contains("_REF1"))
                         {
-                            //Obliczenie przyrostów współrzędnych i odległości między punktami *_REF1 i *_REF2 a punktem wcinanym
-                            //double Name = Convert.ToDouble(XmlDoc.GetElementsByTagName("Name").Item(i - 1 + (CountSum)).InnerText);
-                            double deltaN = Convert.ToDouble(XmlDoc.GetElementsByTagName("North").Item(i-1 + (CountSum)).InnerText) - Convert.ToDouble(XmlDoc.GetElementsByTagName("North").Item(i + 1 + (CountSum)).InnerText);
-                            double deltaE = Convert.ToDouble(XmlDoc.GetElementsByTagName("East").Item(i-1 + (CountSum)).InnerText) - Convert.ToDouble(XmlDoc.GetElementsByTagName("East").Item(i + 1 + (CountSum)).InnerText);
-                            double deltaN2 = Math.Pow(deltaN, 2);
-                            double deltaE2 = Math.Pow(deltaE, 2);
-                            domiar1 = Math.Round(Math.Sqrt(deltaE2 + deltaN2), 4).ToString();
-                            deltaN = Convert.ToDouble(XmlDoc.GetElementsByTagName("North").Item(i  + (CountSum)).InnerText) - Convert.ToDouble(XmlDoc.GetElementsByTagName("North").Item(i + 1 + (CountSum)).InnerText);
-                            deltaE = Convert.ToDouble(XmlDoc.GetElementsByTagName("East").Item(i  + (CountSum)).InnerText) - Convert.ToDouble(XmlDoc.GetElementsByTagName("East").Item(i + 1 + (CountSum)).InnerText);
-                            deltaN2 = Math.Pow(deltaN,2);
-                            deltaE2 = Math.Pow(deltaE, 2);
-                            domiar2 = Math.Round(Math.Sqrt(deltaE2 + deltaN2), 4).ToString();
-                            double elevationPKT = Convert.ToDouble(XmlDoc.GetElementsByTagName("Elevation").Item(i + 1 + (CountSum)).InnerText);
-                            double elevationREF1 = Convert.ToDouble(XmlDoc.GetElementsByTagName("Elevation").Item(i - 1 + (CountSum)).InnerText);
-                            double elevationREF2 = Convert.ToDouble(XmlDoc.GetElementsByTagName("Elevation").Item(i + (CountSum)).InnerText);
-                            if (elevationPKT != (elevationREF1+elevationREF2)/2)
+                            X1 = Convert.ToDouble(reductions.ChildNodes.Item(6).ChildNodes.Item(0).InnerText);
+                            Y1 = Convert.ToDouble(reductions.ChildNodes.Item(6).ChildNodes.Item(1).InnerText);
+                            H1 = Convert.ToDouble(reductions.ChildNodes.Item(6).ChildNodes.Item(2).InnerText);
+                            Ref1Name = reductions.ChildNodes.Item(1).InnerText;
+                        }
+                        if (reductions.ChildNodes.Item(1).InnerText.Contains("_REF2"))
+                        {
+                            X2 = Convert.ToDouble(reductions.ChildNodes.Item(6).ChildNodes.Item(0).InnerText);
+                            Y2 = Convert.ToDouble(reductions.ChildNodes.Item(6).ChildNodes.Item(1).InnerText);
+                            H2 = Convert.ToDouble(reductions.ChildNodes.Item(6).ChildNodes.Item(2).InnerText);
+                            subPKTname = reductions.ChildNodes.Item(1).InnerText.Substring(0, reductions.ChildNodes.Item(1).InnerText.Length - 5);
+                            Ref2Name = reductions.ChildNodes.Item(1).InnerText;
+                        }
+
+                        if (subPKTname != null && reductions.ChildNodes.Item(1).InnerText == subPKTname)
+                        {
+                            X0 = Convert.ToDouble(reductions.ChildNodes.Item(6).ChildNodes.Item(0).InnerText);
+                            Y0 = Convert.ToDouble(reductions.ChildNodes.Item(6).ChildNodes.Item(1).InnerText);
+                            H0 = Convert.ToDouble(reductions.ChildNodes.Item(6).ChildNodes.Item(2).InnerText);
+                        
+                        //Obliczenie przyrostów współrzędnych i odległości między punktami *_REF1 i *_REF2 a punktem wcinanym
+
+                        double deltaN = X0 - X1;
+                        double deltaE = Y0 - Y1;
+                        double deltaN2 = Math.Pow(deltaN, 2);
+                        double deltaE2 = Math.Pow(deltaE, 2);
+                        domiar1 = Math.Round(Math.Sqrt(deltaE2 + deltaN2), 4).ToString();
+                        deltaN = X0 - X2;
+                        deltaE = Y0 - Y2;
+                        deltaN2 = Math.Pow(deltaN, 2);
+                        deltaE2 = Math.Pow(deltaE, 2);
+                        domiar2 = Math.Round(Math.Sqrt(deltaE2 + deltaN2), 4).ToString();
+                        if (H0 != (H1 + H2) / 2)
+                        {
+                            offset1 = (H0 - (H1 + H2) / 2).ToString();
+                        }
+                        XmlNodeList fieldbooklist = XmlDoc.SelectNodes("//FieldBook//PointRecord");
+                            foreach (XmlNode pointrecord in fieldbooklist)
                             {
-                                offset1 = (elevationPKT - (elevationREF1 + elevationREF2) / 2).ToString();
-                            }
-                             //określenie nazwy punktu wcinanego, bez końcówki "_REF1"
-                            string FindName = PKTname.Substring(0, PKTname.Length - 5);
-                            XmlNodeList nodelist = XmlDoc.SelectNodes("//FieldBook//PointRecord");
-                            foreach (XmlNode node in nodelist)
-                            {
-                                foreach (XmlNode child in node.ChildNodes)
+                                foreach (XmlNode child in pointrecord.ChildNodes)
                                 {
-                                    if (child.Name == "Name" && child.InnerText.Equals(FindName))
+                                    if (child.Name == "Name" && child.InnerText.Equals(subPKTname))
                                     {
                                         //wyszukanie odpowiedniego miejsca w xml-u i wprowadzanie danych niezbędnych do zmiany typu na wcięcie
                                         XmlNode EnteredData = null;
@@ -103,15 +130,15 @@ namespace JXL_Converter
                                         XmlNode DistanceTwo = null;
                                         XmlNode VerticalOffset = null;
                                         EnteredData = XmlDoc.CreateNode(XmlNodeType.Element, "EnteredData", "");
-                                        node.AppendChild(EnteredData);
+                                        pointrecord.AppendChild(EnteredData);
                                         Method = XmlDoc.CreateNode(XmlNodeType.Element, "Method", "");
                                         Method.InnerText = "DistanceDistanceIntersection";
                                         EnteredData.AppendChild(Method);
                                         PointOne = XmlDoc.CreateNode(XmlNodeType.Element, "PointOne", "");
-                                        PointOne.InnerText = XmlDoc.GetElementsByTagName("Name").Item(i).InnerText;
+                                        PointOne.InnerText = Ref1Name;
                                         EnteredData.AppendChild(PointOne);
                                         PointTwo = XmlDoc.CreateNode(XmlNodeType.Element, "PointTwo", "");
-                                        PointTwo.InnerText = XmlDoc.GetElementsByTagName("Name").Item(i + 1).InnerText;
+                                        PointTwo.InnerText = Ref2Name;
                                         EnteredData.AppendChild(PointTwo);
                                         DistanceOne = XmlDoc.CreateNode(XmlNodeType.Element, "DistanceOne", "");
                                         DistanceOne.InnerText = domiar1;
@@ -125,7 +152,7 @@ namespace JXL_Converter
                                     }
                                 }
                                 //zmiana metody z COGO, na wcięcie z dwóch punktów
-                                foreach (XmlNode child2 in node.ChildNodes)
+                                foreach (XmlNode child2 in pointrecord.ChildNodes)
                                 {
                                     if (child2.Name == "Method" && child2.InnerText.Contains("CogoCalculated"))
                                     {
@@ -135,7 +162,8 @@ namespace JXL_Converter
                             }
                         }
                     }
-                   
+
+
                     string newfileName = "zmienione" + onlyfileName;
                     string fileSave = Path.Combine(filePath, newfileName);
                     if (File.Exists(fileSave) != true)
@@ -163,7 +191,7 @@ namespace JXL_Converter
                             MessageBox.Show("Plik " + onlyfileName + " nie został dostosowany do importu do C-Geo.", "Konwersja pliku " + onlyfileName + " została zakończona niepowodzeniem.");
                         }
                     }
-                    
+
                 }
 
                 catch (XmlException ex)
@@ -174,4 +202,5 @@ namespace JXL_Converter
         }
     }
 }
+
 
